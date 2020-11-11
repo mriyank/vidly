0ae151a26632711feedcad178ca10c5e6ba704ee
mriyank <mriyanksingh@gmail.com>
2020-11-11 18:57:51 +0530

    added page buttons XD

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -3,19 +3,35 @@ import React, { Component } from "react";
 // Input: liked : boolesn
 //Output: onClick
 
-class Like extends Component {
-  render() {
+const Like = props => {
+    // return (  );
     let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
+    if (!props.liked) classes += "-o";
     return (
       <i
-        onClick={this.props.onClick}
+        onClick={props.onClick}
         style={{ cursor: "pointer" }}
         className={classes}
         aria-hidden="true"
       ></i>
     );
-  }
 }
+ 
+
+
+// class Like extends Component {
+//   render() {
+//     let classes = "fa fa-heart";
+//     if (!this.props.liked) classes += "-o";
+//     return (
+//       <i
+//         onClick={this.props.onClick}
+//         style={{ cursor: "pointer" }}
+//         className={classes}
+//         aria-hidden="true"
+//       ></i>
+//     );
+//   }
+// }
 
 export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -16,13 +19,18 @@ class Movies extends Component {
   handleLike = (movie) => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
-    movies[index] = {...movies[index]};
+    movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
-    this.setState({movies});
+    this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
 
     if (count === 0) return <p>No movies</p>;
 
@@ -65,6 +73,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
