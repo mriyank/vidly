94f949768ac96d5d2e2b119f5702d62c4bd80c11
mriyank <mriyanksingh@gmail.com>
2020-11-15 23:28:12 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import "./App.css";
 import Movies from './components/movies.jsx';
+import Customers from './components/customers.jsx';
+import Rentals from './components/rentals.jsx';
+import NotFound from './components/notFound.jsx';
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Route path="/movies" component={Movies}></Route>
+      <Route path="/customers" component={Customers}></Route>
+      <Route path="/rentals" component={Rentals}></Route>
+      <Route path="/not-found" component={NotFound}></Route>
     </main>
   );
 }
---
+++ src/components/movieForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const movieForm = () => {
+    return ( <h1>movieForm</h1> );
+}
+ 
+export default movieForm;
